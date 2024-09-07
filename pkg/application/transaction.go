ac1930c002028de47682c77c3dcea8898511fada
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2024-09-07 22:33:40 +1200

    feat: filter transactions by account

--- pkg/application/transaction.go
+++ pkg/application/transaction.go
@@ -1,15 +1,25 @@
 package application
 
 import (
+	"github.com/TheQueenIsDead/budge/pkg/database/models"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 func (app *Application) ListTransactions(c echo.Context) error {
-	transactions, err := app.store.Transactions.List()
+
+	account := c.QueryParam("account")
+	var transactions []models.Transaction
+	var err error
+	if account == "" {
+		transactions, err = app.store.Transactions.List()
+	} else {
+		transactions, err = app.store.Transactions.ListByAccount(account)
+	}
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
+
 	accounts, err := app.store.Accounts.List()
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
