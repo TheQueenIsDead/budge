6aa0453a10b562f2a2191db16263f2dfc6efecea
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2024-09-20 14:29:45 +1200

    feat: add form for creating new inventory items

--- pkg/application/inventory.go
+++ pkg/application/inventory.go
@@ -11,3 +11,12 @@ func (app *Application) Inventory(c echo.Context) error {
 		"inventory": inventory,
 	})
 }
+
+func (app *Application) InventoryNew(c echo.Context) error {
+	return c.Render(http.StatusOK, "inventory.new", nil)
+}
+
+func (app *Application) InventoryCreate(c echo.Context) error {
+	// TODO: Wire this up
+	return c.JSON(http.StatusInternalServerError, "not implemented")
+}
