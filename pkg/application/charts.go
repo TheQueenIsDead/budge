6101693b4e28ff840f4c198e7b5ddf62b3d9f01a
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2024-10-02 21:49:42 +1300

    feat: retrieve and format timeseries data for spend over time with configurable periods

--- pkg/application/charts.go
+++ pkg/application/charts.go
@@ -3,42 +3,72 @@ package application
 import (
 	"github.com/TheQueenIsDead/budge/pkg/database/models"
 	"github.com/labstack/echo/v4"
+	"maps"
+	"slices"
+	"time"
 )
 
 type TimeseriesData struct {
 	ChartId    string
 	Title      string
 	Labels     []string
-	Data       []int
+	Data       []float64
 	Border     []string
 	Background []string
 }
 
 func (app *Application) ChartTimeseries(c echo.Context) error {
-	// TODO: Grab actual data over time
+
+	period := c.QueryParam("period")
+	var periodDays = 7
+	switch period {
+	case "week":
+		periodDays = 7
+	case "month":
+		periodDays = 30
+	case "quarter":
+		periodDays = 31 * 4
+	}
+	queryStart := time.Now().AddDate(0, 0, -1*periodDays)
+
+	var transactions []models.Transaction
+	transactions, err := app.store.ReadTransactionsByDate(queryStart, time.Now())
+	if err != nil {
+		c.Logger().Error(err)
+		return err
+	}
+
+	// TODO: Size buckets appropriately for the period, days by default
+	buckets := map[string]float64{}
+	for _, transaction := range transactions {
+		//if transaction.Amount < 0 {
+		key := transaction.Date.Format(time.DateOnly)
+		buckets[key] += transaction.Amount
+		//}
+	}
+
+	var data []float64
+	var labels []string
+	var background []string
+	keys := slices.Collect(maps.Keys(buckets))
+	slices.Sort(keys)
+	for _, k := range keys {
+		data = append(data, buckets[k])
+		labels = append(labels, k)
+		if buckets[k] > 0 {
+			background = append(background, "rgb(26, 188, 156)")
+		} else {
+			background = append(background, "rgb(255, 205, 52)")
+		}
+	}
+
 	return c.Render(200, "chart.timeseries", TimeseriesData{
-		"timeseries_chart",
-		"My First Chart",
-		[]string{"a", "b", "c", "d", "e", "f", "g"},
-		[]int{1, 2, 3, 4, 5, 6, 7},
-		[]string{
-			"rgba(255, 99, 132, 0.2)",
-			"rgba(255, 159, 64, 0.2)",
-			"rgba(255, 205, 86, 0.2)",
-			"rgba(75, 192, 192, 0.2)",
-			"rgba(54, 162, 235, 0.2)",
-			"rgba(153, 102, 255, 0.2)",
-			"rgba(201, 203, 207, 0.2)",
-		},
-		[]string{
-			"rgb(255, 99, 132)",
-			"rgb(255, 159, 64)",
-			"rgb(255, 205, 86)",
-			"rgb(75, 192, 192)",
-			"rgb(54, 162, 235)",
-			"rgb(153, 102, 255)",
-			"rgb(201, 203, 207)",
-		},
+		ChartId:    "timeseries_chart",
+		Title:      "Spend Over Time",
+		Labels:     labels,
+		Data:       data,
+		Border:     background,
+		Background: background,
 	})
 }
 
