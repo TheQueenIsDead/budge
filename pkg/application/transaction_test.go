e4679e7d589e6f833dafbad56d234901b4e2e74c
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2024-11-15 19:28:37 +1300

    test: fix flake from time.now by assigning to var in package

--- pkg/application/transaction_test.go
+++ pkg/application/transaction_test.go
@@ -7,10 +7,12 @@ import (
 	"time"
 )
 
-func Date(s string) time.Time {
+func d(s string) time.Time {
 	date, _ := time.Parse(time.DateOnly, s)
 	return date
 }
+
+var now = time.Now()
 
 func TestFindTransactionRange(t *testing.T) {
 	tests := []struct {
@@ -26,38 +28,38 @@ func TestFindTransactionRange(t *testing.T) {
 		},
 		{"same",
 			[]models.Transaction{
-				{Date: time.Now()},
-				{Date: time.Now()},
+				{Date: now},
+				{Date: now},
 			},
-			models.Transaction{Date: time.Now()},
-			models.Transaction{Date: time.Now()},
+			models.Transaction{Date: now},
+			models.Transaction{Date: now},
 		},
 		{"three",
 			[]models.Transaction{
-				{Date: Date("2000-01-01")},
-				{Date: Date("2000-01-02")},
-				{Date: Date("2000-01-03")},
+				{Date: d("2000-01-01")},
+				{Date: d("2000-01-02")},
+				{Date: d("2000-01-03")},
 			},
-			models.Transaction{Date: Date("2000-01-01")},
-			models.Transaction{Date: Date("2000-01-03")},
+			models.Transaction{Date: d("2000-01-01")},
+			models.Transaction{Date: d("2000-01-03")},
 		},
 		{"three_reverse",
 			[]models.Transaction{
-				{Date: Date("2000-01-03")},
-				{Date: Date("2000-01-02")},
-				{Date: Date("2000-01-01")},
+				{Date: d("2000-01-03")},
+				{Date: d("2000-01-02")},
+				{Date: d("2000-01-01")},
 			},
-			models.Transaction{Date: Date("2000-01-01")},
-			models.Transaction{Date: Date("2000-01-03")},
+			models.Transaction{Date: d("2000-01-01")},
+			models.Transaction{Date: d("2000-01-03")},
 		},
 		{"cross_year",
 			[]models.Transaction{
-				{Date: Date("1000-01-01")},
-				{Date: Date("2000-01-01")},
-				{Date: Date("3000-01-01")},
+				{Date: d("1000-01-01")},
+				{Date: d("2000-01-01")},
+				{Date: d("3000-01-01")},
 			},
-			models.Transaction{Date: Date("1000-01-01")},
-			models.Transaction{Date: Date("3000-01-01")},
+			models.Transaction{Date: d("1000-01-01")},
+			models.Transaction{Date: d("3000-01-01")},
 		},
 	}
 
