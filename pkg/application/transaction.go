49d09759f4db0e6b283e08e2e95048a55552b33a
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2025-03-18 21:37:56 +1300

    feat: add rudimentary report on spend by category to homepage

--- pkg/application/transaction.go
+++ pkg/application/transaction.go
@@ -37,6 +37,53 @@ func (app *Application) ListTransactions(c echo.Context) error {
 	})
 }
 
+func (app *Application) ListTransactionsByCategory(c echo.Context) error {
+
+	period := c.QueryParam("period")
+	var periodDays = 7
+	switch period {
+	case "week":
+		periodDays = 7
+	case "month":
+		periodDays = 30
+	case "quarter":
+		periodDays = 31 * 4
+	}
+	queryStart := time.Now().AddDate(0, 0, -1*periodDays)
+
+	transactions, err := app.store.ReadTransactionsByDate(queryStart, time.Now())
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	slices.SortFunc(transactions, func(a, b models.Transaction) int {
+		return b.Date.Compare(a.Date)
+	})
+
+	type TransactionsByCategory struct {
+		Category     string
+		Total        float64
+		Transactions []models.Transaction
+	}
+	categories := map[string]*TransactionsByCategory{}
+	for _, t := range transactions {
+		category := t.Category.Name
+		if _, ok := categories[category]; ok {
+			categories[category].Transactions = append(categories[category].Transactions, t)
+		} else {
+			categories[category] = &TransactionsByCategory{
+				Category:     category,
+				Total:        t.Amount,
+				Transactions: []models.Transaction{t},
+			}
+		}
+	}
+
+	return c.Render(http.StatusOK, "transactions.by_category", map[string]interface{}{
+		"categories": categoriess,
+	})
+}
+
 func FindTransactionRange(transactions []models.Transaction) (models.Transaction, models.Transaction) {
 
 	if len(transactions) == 0 {
