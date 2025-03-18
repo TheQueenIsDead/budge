584cbc02aa2df91be153a76dbdb970f036fde4eb
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2025-03-18 22:15:16 +1300

    feat: display spend by category in a nicer accordion dropdown

--- pkg/application/transaction.go
+++ pkg/application/transaction.go
@@ -60,18 +60,31 @@ func (app *Application) ListTransactionsByCategory(c echo.Context) error {
 		return b.Date.Compare(a.Date)
 	})
 
+	/*TODO: Filter out apparent zero values / double ups.*/
+	/*TODO: Give uncategorized spending a name (Link to the transactions?)*/
+	/*TODO: Tidy up the accordian drop down, could do a relative date (With hover), as well as justify content in a table?
+	Experiment a bit with not using a <ul> and perhaps having table rows to utilise more div space */
 	type TransactionsByCategory struct {
+		Id           string
 		Category     string
 		Total        float64
 		Transactions []models.Transaction
 	}
 	categories := map[string]*TransactionsByCategory{}
 	for _, t := range transactions {
+
+		// Exit early if the Tx is zero value
+		if t.Amount == 0 {
+			continue
+		}
+
 		category := t.Category.Name
 		if _, ok := categories[category]; ok {
 			categories[category].Transactions = append(categories[category].Transactions, t)
+			categories[category].Total += t.Amount
 		} else {
 			categories[category] = &TransactionsByCategory{
+				Id:           t.Category.Id,
 				Category:     category,
 				Total:        t.Amount,
 				Transactions: []models.Transaction{t},
@@ -80,7 +93,7 @@ func (app *Application) ListTransactionsByCategory(c echo.Context) error {
 	}
 
 	return c.Render(http.StatusOK, "transactions.by_category", map[string]interface{}{
-		"categories": categoriess,
+		"categories": categories,
 	})
 }
 
